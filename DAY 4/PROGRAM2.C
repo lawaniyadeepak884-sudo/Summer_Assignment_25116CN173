85eb86887bee19fe2d3cff4c70cd0f67b817e7a7
lawaniyadeepak884-sudo <lawaniyadeepak884@gmail.com>
2026-06-05 13:43:34 +0530

    check armstrong number

--- DAY 4/PROGRAM2.C
+++ DAY 4/PROGRAM2.C
@@ -1,19 +1,20 @@
 #include <stdio.h>
 int main() {
-    int n, a = 0, b = 1, c, i;
-    printf("Enter n: ");
+    int a = 0, b = 1, c, i = 1, n;
+    printf("Enter a number: ");
     scanf("%d", &n);
-    if (n == 0)
-        printf("Nth term = %d", a);
-    else if (n == 1)
-        printf("Nth term = %d", b);
+    printf("Fibonacci series: ");
+    for (i = 1; i <= n; i++) {
+                printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    if (i == n-1) {
+        printf("enter the number at nh place ");
+    }
     else {
-        for (i = 2; i <= n; i++) {
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        printf("Nth term = %d", b);
+        printf(the number does);
     }
     return 0;
-}
+}q
