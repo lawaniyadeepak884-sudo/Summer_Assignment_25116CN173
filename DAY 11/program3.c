96571be396010ef224b85bccb8edee2086b3cbaa
lawaniyadeepak884-sudo <lawaniyadeepak884@gmail.com>
2026-06-17 09:54:20 +0530

    function to check prime

---
+++ DAY 11/program3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int checkPrime(int n)
+{
+    int i;
+
+    if (n <= 1)
+        return 0;
+
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+         return 0;
+    }
+    return 1;
+}
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (checkPrime(num))
+        printf("%d is a prime number", num);
+    else
+        printf("%d is not a prime number", num);
+    return 0;
+}
