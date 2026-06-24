9e4fd52fbd18bdd674c9dfa5ecb98b3eea72a498
lawaniyadeepak884-sudo <lawaniyadeepak884@gmail.com>
2026-06-24 22:04:40 +0530

    FREQUENCY OF AN ELEMENT IN AN ARRAY

---
+++ DAY 14/program2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int arr[100], n, key, i, count = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Enter element whose frequency is to be found: ");
+    scanf("%d", &key);
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            count++;
+        }
+    }
+    printf("Frequency of %d = %d", key, count);
+    return 0;
+}
